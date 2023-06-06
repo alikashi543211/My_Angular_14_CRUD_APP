1f40c95e60a10064a3f0bb83915699ee30396ad6
kashifalidevstudio <kashifali@devstudio.us>
2023-06-06 17:53:34 +0500

    form group building

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import Swal from 'sweetalert2';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,10 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    title = 'TestAngularUpdated';
-
-    constructor(private _toastr:ToastrService)
+    title = 'Angular14CRUD-App';
+    // regiterForm : FormGroup = new FormGroup({});
+    registerForm : FormGroup;
+    constructor(private _toastr:ToastrService, private _formBuilder:FormBuilder)
     {
 
     }
@@ -18,5 +20,14 @@ export class AppComponent implements OnInit {
     ngOnInit()
     {
 
+    }
+
+    setFromState()
+    {
+        this.registerForm = this._formBuilder.group({
+            id: [0],
+            title: ['', Validators.required],
+            firstName: ['', Validators.required],
+        })
     }
 }
