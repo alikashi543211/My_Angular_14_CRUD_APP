b3f059905b111e04c292fc66644599269aa407bc
kashifalidevstudio <kashifali@devstudio.us>
2023-06-06 17:14:47 +0500

    sweet alert 2 installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 
@@ -16,9 +17,27 @@ export class AppComponent implements OnInit {
 
     ngOnInit()
     {
-        this._toastr.success("Data Saved", "User Master");
-        this._toastr.info("Data Message", "User Master");
-        this._toastr.warning("Warning", "User Master");
-        this._toastr.error("Error", "User Master");
+        // this._toastr.success("Data Saved", "User Master");
+        // this._toastr.info("Data Message", "User Master");
+        // this._toastr.warning("Warning", "User Master");
+        // this._toastr.error("Error", "User Master");
+
+        // Swal.fire('Hello Word');
+        // Swal.fire('Oops...', 'Something went wrong!', 'error')
+        // Swal.fire('Woww...', 'Good', 'success')
+        Swal.fire({
+            title: 'Do you want to save the changes?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              Swal.fire('Saved!', '', 'success')
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
     }
 }
