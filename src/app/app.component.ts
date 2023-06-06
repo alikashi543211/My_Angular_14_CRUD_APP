b6f90314b1ff29217ed977dcc2380bebb6c24714
kashifalidevstudio <kashifali@devstudio.us>
2023-06-06 16:38:54 +0500

    bootstrap fontawesome and  ngx-toastr and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TestAngularUpdated';
+export class AppComponent implements OnInit {
+    title = 'TestAngularUpdated';
+
+    constructor(private _toastr:ToastrService)
+    {
+
+    }
+
+    ngOnInit()
+    {
+        this._toastr.success("Data Saved", "User Master");
+        this._toastr.info("Data Message", "User Master");
+        this._toastr.warning("Warning", "User Master");
+        this._toastr.error("Error", "User Master");
+    }
 }
