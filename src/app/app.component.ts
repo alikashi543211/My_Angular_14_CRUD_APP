883baf4603f5348d0c33925741f3f60c04022a9f
kashifalidevstudio <kashifali@devstudio.us>
2023-06-07 12:06:27 +0500

    registration form design is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,36 +4,41 @@ import { ToastrService } from 'ngx-toastr';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
     title = 'Angular14CRUD-App';
     // regiterForm : FormGroup = new FormGroup({});
-    registerForm : FormGroup;
-    constructor(private _toastr:ToastrService, private _formBuilder:FormBuilder)
-    {
+    registerForm: FormGroup;
+    constructor(private _toastr: ToastrService, private _formBuilder: FormBuilder) {
 
     }
 
-    ngOnInit()
-    {
+    ngOnInit() {
         this.setFromState();
     }
 
-    setFromState()
-    {
+    setFromState() {
         this.registerForm = this._formBuilder.group({
-            id : [0],
-            title : ['', Validators.required],
-            firstName : ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
-            lastName : ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
-            email : ['', Validators.compose([Validators.required, Validators.email])],
-            dob : ['', Validators.compose([Validators.required, Validators.pattern(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)])],
-            password : ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-            confirmPassword : ['', Validators.compose([Validators.required])],
-            acceptTerms : [false, Validators.compose([Validators.required])]
+            id: [0],
+            title: ['', Validators.required],
+            firstName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+            lastName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+            email: ['', Validators.compose([Validators.required, Validators.email])],
+            dob: ['', Validators.compose([Validators.required, Validators.pattern(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)])],
+            password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+            confirmPassword: ['', Validators.compose([Validators.required])],
+            acceptTerms: [false, Validators.compose([Validators.required])]
         })
+    }
+
+    onSubmit() {
+
+    }
+
+    onCancel() {
+        this.registerForm.reset();
     }
 }
