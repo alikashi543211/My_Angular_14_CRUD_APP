36cae32932b397071d33d63ac3ef413d01b55f08
kashifalidevstudio <kashifali@devstudio.us>
2023-06-07 11:08:05 +0500

    form group created on app component ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,15 +19,21 @@ export class AppComponent implements OnInit {
 
     ngOnInit()
     {
-
+        this.setFromState();
     }
 
     setFromState()
     {
         this.registerForm = this._formBuilder.group({
-            id: [0],
-            title: ['', Validators.required],
-            firstName: ['', Validators.required],
+            id : [0],
+            title : ['', Validators.required],
+            firstName : ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+            lastName : ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+            email : ['', Validators.compose([Validators.required, Validators.email])],
+            dob : ['', Validators.compose([Validators.required, Validators.pattern(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)])],
+            password : ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+            confirmPassword : ['', Validators.compose([Validators.required])],
+            acceptTerms : [false, Validators.compose([Validators.required])]
         })
     }
 }
