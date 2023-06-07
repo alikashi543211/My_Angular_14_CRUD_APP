5d45d2f5e20b7ed7f032ee44526fac0d2bb7332d
kashifalidevstudio <kashifali@devstudio.us>
2023-06-07 12:30:44 +0500

    database in memory web api created

---
+++ src/app/_helpers/data.service.ts
@@ -0,0 +1,22 @@
+import { User } from './user.interface';
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService implements InMemoryDbService {
+
+    constructor() { }
+
+    createDb() {
+        let users: User[] = [
+            { id: 1, title: 'Mr', firstName: 'Ajeet', lastName: 'Singh', dob: '2000-05-15', email: 'ajeet@test.com', password: '12345678', acceptTerms: true },
+            { id: 1, title: 'Mr', firstName: 'Chandan', lastName: 'Singh', dob: '2002-08-20', email: 'chandan@test.com', password: '12345678', acceptTerms: true },
+        ];
+
+        return { users };
+    }
+
+
+}
